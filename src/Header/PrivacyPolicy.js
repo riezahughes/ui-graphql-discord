9ed2c7c4d67f5950d94a608ac7c8e56c4ff07149
Dan <dan@ekwholesale.co.uk>
2019-05-25 23:33:39 +0100

    adjustments to the responsivity

--- src/Header/PrivacyPolicy.js
+++ src/Header/PrivacyPolicy.js
@@ -11,7 +11,7 @@ constructor(props){
 hideMe(e){
     document.getElementById("top-page-bar").style.opacity = 0;
     setTimeout(function(){
-        document.getElementById("top-page-bar").style.height = 0;
+        document.getElementById("top-page-bar").style.height = 30;
     }, 500)
 
 }
@@ -24,7 +24,7 @@ leaveMe(e){
         return(
             <div className="row" id="top-page-bar">
                 <div className="col-12">
-                    <small>This site uses cookies<span class="hide-on-mobile"> in order to functioncorrectly. Please confirm it's alright so we don't have to kick you.</span></small>&nbsp;<small><button onClick={this.hideMe} className="discord-btn-yes ml-1" type="button">I Agree</button> <button onClick={this.leaveMe} className="discord-btn-no ml-1" type="button">Begone Thot</button></small>
+                    <small className="privacy-text">This site uses cookies in order to function <span class="hide-on-mobile">correctly. Please confirm it's alright so we don't have to kick you.</span></small>&nbsp;<small><button onClick={this.hideMe} className="discord-btn-yes ml-1" type="button">I Agree</button> <button onClick={this.leaveMe} className="discord-btn-no ml-1" type="button">Begone Thot</button></small>
                 </div>
             </div>
         );
