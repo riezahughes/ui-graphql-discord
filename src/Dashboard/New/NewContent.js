617e64b690fa668af426640b046424d25b55a60a
Dan <dan@ekwholesale.co.uk>
2019-06-01 10:39:31 +0100

    Form markup for new item

--- src/Dashboard/New/NewContent.js
+++ src/Dashboard/New/NewContent.js
@@ -5,11 +5,81 @@ export default class NewContent extends Component{
 
 constructor(props){
     super(props);
+    this.state = {
+        "name": "",
+        "file": "",
+        "tag": "",
+        "hotkey": ""
+    }
+    this.enterText = this.enterText.bind(this);
+    this.enterTag = this.enterTag.bind(this);
+    this.enterHotkey = this.enterHotkey.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+    this.onUploadChoice = this.onUploadChoice.bind(this);
 }   
+
+
+
+enterText(e){
+    this.setState({
+        "name": e.target.value
+    });
+}
+
+onSubmit(e){
+    e.preventDefault();
+}
+
+onUploadChoice(e){
+    this.setState({
+        "file": e.target.files[0]
+    })
+}
+
+enterTag(e){
+    this.setState({
+        "tag": e.target.value
+    })
+}
+
+enterHotkey(e){
+    this.setState({
+        "hotkey": e.target.value
+    })
+}
 
     render(){
         return(
-            <h1>New</h1>
+            <div id="createNew" className="text-left">
+                <form onSubmit={this.onSubmit}>
+                    <div class="form-row">
+                        <div className="form-group col-4">
+                            <label for="newname">Title:</label>
+                            <input className="form-control" type="text" value={this.state.name} onChange={this.enterText} id="newname" name="newname"/>
+                        </div>
+                        <div className="form-group col-4">
+                            <label for="tagChoice">Tag:</label>
+                            <select className="form-control" name="tagChoice" id="tagChoice" onChange={this.enterTag}>
+                                <option>HarryPotter</option>
+                                <option>test</option>
+                                <option>ffxiv</option>
+                            </select>
+                        </div>
+                        <div className="form-group col-4">
+                            <label for="hotkeyChoice">Hotkey:</label>
+                            <input className="form-control" type="text" name="hotkeyChoice" id="hotkeyChoice" onChange={this.enterHotkey} value={this.state.hotkey} />
+                        </div>
+                    </div>
+                    <div class="form-row">
+                        <div className="form-group col-6">
+                            <input className="form-control" type="file" id="soundUpload" name="soundUpload" onChange={this.onUploadChoice}/>
+                        </div>
+                        <div className="form-group col-6">
+                            <button clasName="btn btn-fluid form-control w-100" type="button" id="submitNewSound" name="submitNewSound">Create</button>
+                        </div>
+                    </div>
+                </form>
+            </div>
         );
     }
 }
